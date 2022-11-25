983fbb46f5806e1869882b26318b7bf8395b19b9
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-11-25 16:41:36 +0530

    redirecting requests

--- Node JS/NodeJS_Basics/app.js
+++ Node JS/NodeJS_Basics/app.js
@@ -1,9 +1,11 @@
 // create a server
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //   process.exit();
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
@@ -11,6 +13,12 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-type', 'text/html');
