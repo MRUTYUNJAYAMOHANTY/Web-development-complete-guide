65265130b61406a6e9938511631c17e1de4d5298
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-11-25 18:28:49 +0530

    parsing request bodies

--- Node JS/NodeJS_Basics/app.js
+++ Node JS/NodeJS_Basics/app.js
@@ -1,9 +1,7 @@
-// create a server
 const http = require('http');
 const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
   const url = req.url;
   const method = req.method;
   if (url === '/') {
@@ -16,16 +14,26 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
-  res.setHeader('Content-type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my node js server!</h1></body>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
 });
+
 server.listen(3000);
--- Node JS/README.md
+++ Node JS/README.md
@@ -2,3 +2,5 @@
 
 spread - To pull elements or properties out of arrays or objects
 rest - To merge multiple arguments into an array,used argument list of a function
+
+Type node app.js to run the compile and run file
