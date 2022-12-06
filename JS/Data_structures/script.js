c9428082bd00ae1b4eebbc86147a7e063074bfaa
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-06 20:36:26 +0530

    rest pattern and parameters

--- JS/Data_structures/script.js
+++ JS/Data_structures/script.js
@@ -39,6 +39,10 @@ const restaurant = {
       `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 ///////////////////////////////////////
@@ -144,3 +148,33 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+
+///////////////////////////////////////
+// Rest Pattern and Parameters
+// 1) Destructuring
+// SPREAD, because on RIGHT side of =
+const arr2 = [1, 2, ...[3, 4]];
+// REST, because on LEFT side of =
+const [a2, b2, ...others] = [1, 2, 3, 4, 5];
+console.log(a2, b2, others);
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+const x1 = [23, 5, 7];
+add(...x1);
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
