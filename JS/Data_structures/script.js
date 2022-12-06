e94183cbae85cc722f56cbf10d9b2aeb1125ba32
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-06 12:26:33 +0530

    destructuring arrays

--- JS/Data_structures/script.js
+++ JS/Data_structures/script.js
@@ -26,4 +26,37 @@ const restaurant = {
       close: 24,
     },
   },
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+///////////////////////////////////////
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
