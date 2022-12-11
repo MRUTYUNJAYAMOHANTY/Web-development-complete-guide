5aa26723c4d247fa94d167d91e24641ca5987aa0
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-11 18:37:15 +0530

    handling different routes

--- Node JS/complete_NodeJS_guide/app.js
+++ Node JS/complete_NodeJS_guide/app.js
@@ -1,9 +1,22 @@
 const http = require('http');
-
-const routes = require('./routes');
-
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+//const routes = require('./routes');
+//console.log(routes.someText);
+const express = require('express');
+const app = express();
+//const server = http.createServer(routes.handler);
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
+  next(); //Allows the request to continue the next middleware inline
+});
+app.use('/add-product', (req, res, next) => {
+  console.log('In the first middleware');
+  // next(); //Allows the request to continue the next middleware inline
+  res.send('<h1> The "Product tag" page </h1>');
+});
+app.use('/', (req, res, next) => {
+  console.log('In the second middleware');
+  res.send('<h1>Hello from express js!</h1>');
+});
+const server = http.createServer(app);
+app.listen(3000);
+//server.listen(3000);
