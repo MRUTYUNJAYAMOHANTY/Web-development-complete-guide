7f018869a8a17874175f7fd7e83642791abae539
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-13 22:59:19 +0530

    fetching initial posts

--- MEAN/my-messages/api/app.js
+++ MEAN/my-messages/api/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-  console.log(`First middleware`);
-  next();
-});
-app.use((req, res, next) => {
-  res.send(`sent to client - Hello from express`);
+// app.use((req, res, next) => {
+//   console.log(`First middleware`);
+//   next();
+// });
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fadfswe',
+      title: 'first-server-side-posts',
+      content: 'This is coming from server',
+    },
+    {
+      id: 'ksuieqse',
+      title: 'second-server-side-posts',
+      content: 'This is coming from another server',
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts,
+  });
 });
 module.exports = app;
