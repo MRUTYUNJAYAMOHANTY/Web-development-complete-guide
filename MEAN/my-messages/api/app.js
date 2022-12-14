bf35df516681c5d97cf6f625c730651b1ff68cd8
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-14 16:20:39 +0530

    adding post backend point

--- MEAN/my-messages/api/app.js
+++ MEAN/my-messages/api/app.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // CORS MIDDLEWARE
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With',
-    'Content-Type',
-    'Accept'
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET,POST,PATCH,PUT,DELETE,OPTIONS'
+    'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
+});
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Posts added successfully',
+  });
 });
 app.use('/api/posts', (req, res, next) => {
   const posts = [
--- MEAN/my-messages/src/app/posts/posts.service.ts
+++ MEAN/my-messages/src/app/posts/posts.service.ts
@@ -27,7 +27,12 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
