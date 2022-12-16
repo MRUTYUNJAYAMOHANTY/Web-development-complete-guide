68222a709c45c467a899aa21002cc17c319dc379
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-16 19:16:38 +0530

    style links

--- MEAN/my-messages/src/app/app-routing.module.ts
+++ MEAN/my-messages/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: PostListComponent},
+  {path:'create',component:PostCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
