49d7fff286d61ae57095dc82af2d02eb012d5491
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2022-12-21 14:58:55 +0530

    dir-change

--- MEAN/my-messages/api/app.js
+++ MEAN/my-messages/api/app.js
@@ -24,7 +24,7 @@ mongoose
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use("/images", express.static(path.join("images")));
+  app.use("/images", express.static(path.join("api/images")));
   
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
