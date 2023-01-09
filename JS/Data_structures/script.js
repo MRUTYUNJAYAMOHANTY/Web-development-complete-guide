c80fb23f04432d0cd45c64ba757b1b2cc7b6ff79
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-01-09 16:39:26 +0530

    The Nullish Coalescing Operator

--- JS/Data_structures/script.js
+++ JS/Data_structures/script.js
@@ -202,3 +202,12 @@ if (restaurant.orderPizza) {
   restaurant.orderPizza('mushrooms', 'spinach');
 }
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
