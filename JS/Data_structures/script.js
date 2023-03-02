7af24f8a3afde708fdbbf6ab1b013d0e6d67d142
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-03-02 18:42:46 +0530

    optional chaining

--- JS/Data_structures/script.js
+++ JS/Data_structures/script.js
@@ -249,6 +249,30 @@ for (const [i, el] of menuof.entries()) {
 }
 // console.log([...menu.entries()]);
 
+///////////////////////////////////////
+// Optional Chaining
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+// console.log(restaurant.openingHours.mon.open);
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+console.log(users[0]?.name ?? 'User array empty');
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
 
 
 
