cd09ad6d448efd9ca1c96662d70ebd4c7c931a09
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-05-24 14:59:24 +0530

    onclick event

--- React JS/Complete_React_Guide/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ React JS/Complete_React_Guide/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,18 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 // we can pass any name as props,below we pass  
 const ExpenseItem =(props) =>{
-    
+    let title = props.name
+    const clickHandler = () => {
+        console.log('clicked!!!');
+    }
     return (
         <Card className='expense-item'>
              <ExpenseDate time={props.time}/>
             <div className='expense-item__description'>
-                <h2>{props.name}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>₹{props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
