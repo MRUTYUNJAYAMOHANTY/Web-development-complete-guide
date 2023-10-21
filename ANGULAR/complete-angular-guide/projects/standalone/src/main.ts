449941204e681c570e0241827250471198485746
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-10-21 21:35:03 +0530

    code formated

--- ANGULAR/complete-angular-guide/projects/standalone/src/main.ts
+++ ANGULAR/complete-angular-guide/projects/standalone/src/main.ts
@@ -1,26 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { environment as env_receipeshopping } from '../../recipe_shopping-list/src/environments/environment';
-// import { AppModule } from './app/app.module'; Not required if we are choosing standalone concept  
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { AnalyticsService } from './app/shared/analytics.service';
 import { AppRoutingModule } from './app/app-routing.module';
-
-/*
-   From version 14 standalone component used in application . And It is not required to bootstrap appmodule.
-   Beacuse root component 'AppComponent' is standalone  
-
-    platformBrowserDynamic().bootstrapModule(AppModule)
-      .catch(err => console.error(err));
-*/
-
-
-if (env_receipeshopping.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent,{
   providers: [importProvidersFrom(AppRoutingModule)]
-  } // AnalyticsService can be declared in providers array. But not needed
+  }
 );
