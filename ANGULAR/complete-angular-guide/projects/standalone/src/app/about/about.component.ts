24971376d9302f2059b3b45cf3f1c0fc601edf39
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-10-21 21:41:00 +0530

    Resolved NG0912: Component ID generation collision detected

--- ANGULAR/complete-angular-guide/projects/standalone/src/app/about/about.component.ts
+++ ANGULAR/complete-angular-guide/projects/standalone/src/app/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone:true,
+  selector: 'app-about',
   templateUrl: './about.component.html'
 })
 export class AboutComponent {}
--- ANGULAR/complete-angular-guide/projects/standalone/src/app/dashboard/today/today.component.ts
+++ ANGULAR/complete-angular-guide/projects/standalone/src/app/dashboard/today/today.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
+  selector: 'app-today',
   templateUrl: './today.component.html',
 })
 export class TodayComponent {}
