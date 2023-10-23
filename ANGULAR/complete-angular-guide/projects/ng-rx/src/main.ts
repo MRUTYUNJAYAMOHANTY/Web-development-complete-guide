fdf78975f8b29ea69092e014144818ea04e60c17
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-10-23 11:12:52 +0530

    store registered in main.ts

--- ANGULAR/complete-angular-guide/projects/ng-rx/src/main.ts
+++ ANGULAR/complete-angular-guide/projects/ng-rx/src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
+import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent,{
+    providers: [provideStore()]
+});
