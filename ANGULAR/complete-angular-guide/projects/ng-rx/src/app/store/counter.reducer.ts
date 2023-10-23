6d8f1abc3c120e4972e4a0899ee4d3b5358b2fb4
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-10-23 19:58:11 +0530

    Handling actions without create reducers

--- ANGULAR/complete-angular-guide/projects/ng-rx/src/app/store/counter.reducer.ts
+++ ANGULAR/complete-angular-guide/projects/ng-rx/src/app/store/counter.reducer.ts
@@ -3,11 +3,14 @@ import { increment } from "./counter.action";
 
 const initialSte = 0;
 
-export const counterReducer = createReducer(
-    initialSte,
-    on(increment,(state, action)=> state + action.value)
-)
+// export const counterReducer = createReducer(
+//     initialSte,
+//     on(increment,(state, action)=> state + action.value)
+// )
 
-// export function counterReducer(state = initialSte){
-//     return state;
-// }
+// reducer can be created as a standard function
+
+export function counterReducer(state = initialSte, action: any){
+    if(action.type === '[Counter] Increment')
+    return state + action.value;
+}
