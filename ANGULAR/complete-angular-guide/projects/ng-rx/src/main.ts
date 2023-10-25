8d6654439629234373654d4c05c7cf763520d92d
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2023-10-25 20:22:21 +0530

    efffects added

--- ANGULAR/complete-angular-guide/projects/ng-rx/src/main.ts
+++ ANGULAR/complete-angular-guide/projects/ng-rx/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent,{
-    providers: [provideStore({counter: counterReducer})]
+    providers: [provideStore({counter: counterReducer}), provideEffects()]
 });
