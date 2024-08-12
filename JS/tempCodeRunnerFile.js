5492a59c7111b409cd39a864479749616071ece3
MRUTYUNJAYA MOHANTY <me.mrutyunjaya@gmail.com>
2024-08-12 15:42:04 +0530

    js

---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+console.log(1 + +"2" + "2");
+
+// console.log(1 + -"1" + "2");
+
+// console.log(+"1" + "1" + "2");
+
+// console.log("A" - "B" + "2");
+
+// console.log("A" - "B" + 2);
---
+++ JS/test.js
@@ -0,0 +1,7 @@
+// for (var i = 0; i < 5; i++){
+//     var btn = document.createElement('button');
+//     btn.appendChild(document.createTextNode('Button' + i));
+//     btn.addEventListener('click', function(){ console.log(i);});
+//     document.body.appendChild(btn);    
+//    }
+
